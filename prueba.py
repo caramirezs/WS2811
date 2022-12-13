c9e56bac7f8614c39e38a8bfeafda8bd8528c212
caramirezs <kamandramsan@gmail.com>
2022-12-12 19:37:31 -0500

    prueba

--- prueba.py
+++ prueba.py
@@ -4,13 +4,15 @@ import neopixel
 import re
 import math
 
-n_strips = 1
-N_PIXEL = 50 * n_strips
-PIXEL_PIN = board.D18
-BRIGHTNESS=0.5
-ORDER = neopixel.RGB
-
-pixels = neopixel.NeoPixel(PIXEL_PIN, N_PIXEL, auto_write=False,
-                           brightness=BRIGHTNESS, pixel_order=ORDER)
-
-pixels.fill((0, 255, 0))
+# n_strips = 1
+# N_PIXEL = 50 * n_strips
+# PIXEL_PIN = board.D18
+# BRIGHTNESS=0.5
+# ORDER = neopixel.RGB
+#
+# pixels = neopixel.NeoPixel(PIXEL_PIN, N_PIXEL, auto_write=False,
+#                            brightness=BRIGHTNESS, pixel_order=ORDER)
+#
+# pixels.fill((0, 255, 0))
+pixels = neopixel.NeoPixel(board.D18, 30)
+pixels[0] = (255, 0, 0)
