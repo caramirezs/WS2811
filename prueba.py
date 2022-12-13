28553e44321e621c6d84f2b60578e6421f364b15
caramirezs <kamandramsan@gmail.com>
2022-12-12 19:22:13 -0500

    prueba

--- prueba.py
+++ prueba.py
@@ -1,3 +1,16 @@
-2+2
-3+5
-1+1
+import time
+import board
+import neopixel
+import re
+import math
+
+n_strips = 1
+N_PIXEL = 50 * n_strips
+PIXEL_PIN = board.D18
+BRIGHTNESS=0.5
+ORDER = neopixel.RGB
+
+pixels = neopixel.NeoPixel(PIXEL_PIN, N_PIXEL, auto_write=False,
+                           brightness=BRIGHTNESS, pixel_order=ORDER)
+
+pixels.fill((0, 255, 0))
